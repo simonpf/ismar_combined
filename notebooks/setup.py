c988c850611cb84a512b304f33ad737f09570479
simonpf <simonpf@chalmers.se>
2018-12-06 17:54:48 +0100

    Added setup script for notebooks.

---
+++ notebooks/setup.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from IPython import get_ipython
+ip = get_ipython()
+
+ip.magic("%matplotlib inline")
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+plt.style.use("/home/simon/.config/matplotlib/notebook")
