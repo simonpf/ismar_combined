c1a4469a120ae3b083afe6985e9cce1b02a15d7c
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2019-09-20 20:06:25 +0200

    Fix lookup of joint flight path from environment variables.

--- joint_flight/__init__.py
+++ joint_flight/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 if "JOINT_FLIGHT_PATH" in os.environ:
-    path = "JOINT_FLIGHT_PATH"
+    path = os.environ["JOINT_FLIGHT_PATH"]
 else:
     path = os.path.join(os.path.dirname(__file__), "..")
     print("No environment variable JOINT_FLIGHT_PATH found. Will look in {} "
