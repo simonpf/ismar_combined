21996ba2b827b2a0fd2b8267800bf4a057106ff1
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2021-03-21 18:38:02 +0100

    Added module to load data from FAAM CloudSat underpasses.

--- joint_flight/__init__.py
+++ joint_flight/__init__.py
@@ -1,8 +1,9 @@
 import os
+from pathlib import Path
 
 if "JOINT_FLIGHT_PATH" in os.environ:
-    path = os.environ["JOINT_FLIGHT_PATH"]
+    path = Path(os.environ["JOINT_FLIGHT_PATH"])
 else:
-    path = os.path.join(os.path.dirname(__file__), "..")
+    path = Path(__file__).parent.parent
     print("No environment variable JOINT_FLIGHT_PATH found. Will look in {} "
           " for the joint flight data.".format(path))
